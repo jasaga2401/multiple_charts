534f17e7c911a8db10b93da78938e0c087ae9b62
g.allcock <g.allcock@LC212TUTORXX>
2023-11-24 10:55:54 +0000

    Add project files.

---
+++ multiple_charts/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace multiple_charts
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea14 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend14 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series14 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea14.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea14);
+            legend14.Name = "Legend1";
+            this.chart1.Legends.Add(legend14);
+            this.chart1.Location = new System.Drawing.Point(218, 40);
+            this.chart1.Name = "chart1";
+            series14.ChartArea = "ChartArea1";
+            series14.Legend = "Legend1";
+            series14.Name = "Series1";
+            this.chart1.Series.Add(series14);
+            this.chart1.Size = new System.Drawing.Size(694, 401);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(39, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Column";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(39, 103);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 39);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Line";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(39, 157);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(136, 40);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Bar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(39, 213);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(136, 40);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Pie";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(39, 269);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(136, 40);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Area";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click_1);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(39, 324);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(136, 37);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Donaught";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click_1);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(39, 376);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(136, 41);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Point";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(955, 495);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ multiple_charts/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace multiple_charts
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+           
+                   
+
+            private void Form1_Load(object sender, EventArgs e)
+            {
+
+            }
+                       
+
+            static List<KeyValuePair<string, double>> define_list()
+            {
+                List<KeyValuePair<string, double>> temperatureData = new List<KeyValuePair<string, double>>
+            {
+                new KeyValuePair<string, double>("2018",9.49),
+                new KeyValuePair<string, double>("2019",9.42),
+                new KeyValuePair<string, double>("2020",9.62),
+                new KeyValuePair<string, double>("2021",9.28),
+                new KeyValuePair<string, double>("2022",10.93)
+            };
+                return temperatureData;
+            }
+
+            static Series define_series()
+            {
+                Series series = new Series("Average Temp in degrees C");
+                return series;
+            }                 
+
+                       
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            chart1.Series.Clear();
+            List<KeyValuePair<string, double>> temperatureData = define_list();
+            Series series = define_series();
+            series.ChartType = SeriesChartType.Column;
+            foreach (var temp in temperatureData)
+            {
+                series.Points.AddXY(temp.Key, temp.Value);
+            }
+
+            chart1.Series.Add(series);
+
+            chart1.ChartAreas[0].AxisX.Title = "Year";
+            chart1.ChartAreas[0].AxisY.Title = "Temperature in degrees C";
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            chart1.Series.Clear();
+            List<KeyValuePair<string, double>> temperatureData = define_list();
+            Series series = define_series();
+            series.ChartType = SeriesChartType.Line;
+            foreach (var temp in temperatureData)
+            {
+                series.Points.AddXY(temp.Key, temp.Value);
+            }
+
+            chart1.Series.Add(series);
+
+            chart1.ChartAreas[0].AxisX.Title = "Year";
+            chart1.ChartAreas[0].AxisY.Title = "Temperature in degrees C";
+
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            chart1.Series.Clear();
+            List<KeyValuePair<string, double>> temperatureData = define_list();
+
+            Series series = define_series();
+            series.ChartType = SeriesChartType.Bar;
+
+            foreach (var temp in temperatureData)
+            {
+                series.Points.AddXY(temp.Key, temp.Value);
+            }
+
+            chart1.Series.Add(series);
+
+            chart1.ChartAreas[0].AxisX.Title = "Year";
+            chart1.ChartAreas[0].AxisY.Title = "Temperature in degrees C";
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            chart1.Series.Clear();
+            List<KeyValuePair<string, double>> temperatureData = define_list();
+
+            Series series = define_series();
+            series.ChartType = SeriesChartType.Pie;
+
+            foreach (var temp in temperatureData)
+            {
+                series.Points.AddXY(temp.Key, temp.Value);
+            }
+
+            chart1.Series.Add(series);
+
+            chart1.ChartAreas[0].AxisX.Title = "Year";
+            chart1.ChartAreas[0].AxisY.Title = "Temperature in degrees C";
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            chart1.Series.Clear();
+            List<KeyValuePair<string, double>> temperatureData = define_list();
+
+            Series series = define_series();
+            series.ChartType = SeriesChartType.Area;
+
+            foreach (var temp in temperatureData)
+            {
+                series.Points.AddXY(temp.Key, temp.Value);
+            }
+
+            chart1.Series.Add(series);
+
+            chart1.ChartAreas[0].AxisX.Title = "Year";
+            chart1.ChartAreas[0].AxisY.Title = "Temperature in degrees C";
+        }
+
+        private void button6_Click_1(object sender, EventArgs e)
+        {
+            chart1.Series.Clear();
+            List<KeyValuePair<string, double>> temperatureData = define_list();
+
+            Series series = define_series();
+            series.ChartType = SeriesChartType.Doughnut;
+
+            foreach (var temp in temperatureData)
+            {
+                series.Points.AddXY(temp.Key, temp.Value);
+            }
+
+            chart1.Series.Add(series);
+
+            chart1.ChartAreas[0].AxisX.Title = "Year";
+            chart1.ChartAreas[0].AxisY.Title = "Temperature in degrees C";
+        }
+
+        private void button7_Click_1(object sender, EventArgs e)
+        {
+            chart1.Series.Clear();
+            List<KeyValuePair<string, double>> temperatureData = define_list();
+
+            Series series = define_series();
+            series.ChartType = SeriesChartType.Point;
+
+            foreach (var temp in temperatureData)
+            {
+                series.Points.AddXY(temp.Key, temp.Value);
+            }
+
+            chart1.Series.Add(series);
+
+            chart1.ChartAreas[0].AxisX.Title = "Year";
+            chart1.ChartAreas[0].AxisY.Title = "Temperature in degrees C";
+        }
+    }
+}
